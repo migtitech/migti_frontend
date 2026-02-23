b903f55bf4dc84ef7f85c2e22a04e85061600eb6
Anuradha-Sisodiya <anuradhasisodiya21@gmail.com>
2026-02-23 11:33:26 +0530

    remove the location icon in the company list from the companies

--- src/views/admin/CompanyList.js
+++ src/views/admin/CompanyList.js
@@ -13,7 +13,6 @@ import {
   CTableHeaderCell,
   CTableRow,
   CButton,
-  CBadge,
   CAlert,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
@@ -137,18 +136,6 @@ const CompanyList = () => {
                             onClick={() => navigate(`/companies/${id}`)}
                           >
                             <CIcon icon={cilZoom} />
-                          </CButton>
-
-                          <CButton
-                            color="primary"
-                            variant="ghost"
-                            size="sm"
-                            title="Branches"
-                            onClick={(e) => {
-                              e.stopPropagation()
-                              navigate(`/companies/${id}/branches`)}}
-                          >
-                            <CIcon icon={cilLocationPin} />
                           </CButton>
 
                           {canUpdate('companies') && (
